df3abca1356d009278dc8ba5d7a2804dbe5ccac1
nehalgupta25 <guptanehal578@gmail.com>
2026-08-04 13:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_communit.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
